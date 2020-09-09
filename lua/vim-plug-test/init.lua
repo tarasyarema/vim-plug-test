8c2bea096505df0e7d7344f1f18024f2396866e8
2pac <tarasyarema@pm.me>
2020-09-09 20:11:42 +0200

    fun

--- lua/vim-plug-test/init.lua
+++ lua/vim-plug-test/init.lua
@@ -1,6 +1,11 @@
-local function printHello()
-    print("Hi!")
+local fun = require('vim-plug-test.lib.fun')
+local api = vim.api
 
+local function printHello()
+    local linenr = api.nvim_win_get_cursor(0)[1]
+    local curline = api.nvim_buf_get_lines(0, linenr-1, linenr, false)
+    print(curline)
+    fun.fun()
 end
 
 return {
